dbb9f8e437090511563b78b5c23f76fd2b4d3c69
Salman <salman.hafeez@sprintx.net>
2025-03-04 15:28:48 +0500

    fix url

--- pages/api/auth/clickfunnels.js
+++ pages/api/auth/clickfunnels.js
@@ -1,7 +1,6 @@
 export default function handler(req, res) {
   const CLIENT_ID = process.env.CLICKFUNNELS_CLIENT_ID;
-  const REDIRECT_URI =
-    process.env.NEXT_PUBLIC_BACKEND_URL + "/api/auth/callback";
+  const REDIRECT_URI = process.env.CLICKFUNNELS_REDIRECT_URI;
 
   const authUrl = `https://accounts.myclickfunnels.com/oauth/authorize?client_id=${CLIENT_ID}&grant_type=authorization_code&redirect_uri=${REDIRECT_URI}&response_type=code&new_installation=true`;
 
